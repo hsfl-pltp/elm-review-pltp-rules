6f68034fcaad9c1b1d20e78f44239cc8174ff8a9
Hauke <hauing@pm.me>
2021-11-16 20:46:09 +0100

    Added a list with all default imports as example

--- src/CoreImport.elm
+++ src/CoreImport.elm
@@ -1,6 +1,18 @@
 module CoreImport exposing (..)
 
-import List exposing (map)
+import Basics exposing (..)
+import List exposing (List, (::))
+import Maybe exposing (Maybe(..))
+import Result exposing (Result(..))
+import String exposing (String)
+import Char exposing (Char)
+import Tuple
+
+import Debug
+
+import Platform exposing ( Program )
+import Platform.Cmd as Cmd exposing ( Cmd )
+import Platform.Sub as Sub exposing ( Sub )
 
 a : List String
 a = ["b"]
