68a0976e1accdc108be58e49a913e5a328e58a84
Hauke <hauing@pm.me>
2021-11-21 13:41:55 +0100

    Refactor test for NoCoreModuleImports

--- review/tests/NoCoreModuleImportsTest.elm
+++ review/tests/NoCoreModuleImportsTest.elm
@@ -1,37 +1,52 @@
 module NoCoreModuleImportsTest exposing (all)
 
 import NoCoreModuleImports exposing (rule)
-import Review.Test
+import Review.Test exposing (ExpectedError)
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
-    describe "Test"
-        [ test "should report an error when a core module is imported" <|
-            \() ->
-                source
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error expectedError ]
+    describe "NoCoreModuleImports"
+        [ testRule "Basics"
+        , testRule "List"
+        , testRule "Maybe"
+        , testRule "Result"
+        , testRule "String"
+        , testRule "Char"
+        , testRule "Tuple"
+        , testRule "Debug"
+        , testRule "Platform"
+        , testRule "Platform.Cmd"
+        , testRule "Platform.Sub"
         ]
 
 
-expectedError : { message : String, details : List String, under : String }
-expectedError =
-    { message = "Import of core module found : List"
-    , details =
-        [ "The import of a core module is not necessary, because they are imported by default."
-        , "For a list of all default imports take a look at https://package.elm-lang.org/packages/elm/core/latest/."
-        ]
-    , under = "import List exposing (map)"
-    }
+testRule : String -> Test
+testRule name =
+    test ("Should report an error when the module " ++ name ++ "is imported.")
+        (\_ ->
+            source name
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors [ assertError name ]
+        )
 
 
-source : String
-source =
+assertError : String -> ExpectedError
+assertError name =
+    Review.Test.error
+        { message = "Import of core module found : " ++ name
+        , details =
+            [ "The import of a core module is not necessary, because they are imported by default."
+            , "For a list of all default imports take a look at https://package.elm-lang.org/packages/elm/core/latest/."
+            ]
+        , under = "import " ++ name ++ " exposing (..)"
+        }
+
+
+source : String -> String
+source name =
     """
 module A exposing(..)
 
-import List exposing (map)
-"""
+import """ ++ name ++ " exposing (..)"
