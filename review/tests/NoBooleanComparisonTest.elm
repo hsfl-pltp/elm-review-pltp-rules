aa41d05c6125435edd35f6b0686f6846445d095b
Hauke <hauing@pm.me>
2021-11-21 13:31:18 +0100

    Update test for NoBooleanComprasion

--- review/tests/NoBooleanComparisonTest.elm
+++ review/tests/NoBooleanComparisonTest.elm
@@ -1,49 +1,28 @@
 module NoBooleanComparisonTest exposing (all)
 
 import NoBooleanComparison exposing (rule)
-import Review.Test
+import Review.Test exposing (ExpectedError)
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
     describe "NoBooleanComparison"
-        [ test "should report an error when a Value is compared to \"True\"" <|
-            \() ->
-                (source "bar == True")
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error (expectedError "bar == True") ]
-        , test "should report an error when a \"True\" is compared to a value" <|
-            \() ->
-                (source "True == bar")
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error (expectedError "True == bar") ]
-        , test "should report an error when a value is compared to \"False\"" <|
-            \() ->
-                (source "bar == False")
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error (expectedError "bar == False") ]
-        , test "should report an error when a Boolean is compared to a value" <|
-            \() ->
-                (source "False == bar")
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error (expectedError "False == bar") ]
+        [ testRule "bar == True"
+        , testRule "bar == False"
+        , testRule "True == bar"
+        , testRule "False == bar"
         ]
 
 
-expectedError : String -> { message : String, details : List String, under : String }
-expectedError under =
-    { message = "Detected a comparison with boolean"
-    , details =
-        [ "There is no need to compare a value of Type Boolean with \"True\" or \"False\""
-        , "For Example: \"if b == True then .. else ..\" is the same as \"if b then ... else ...\", "
-        ]
-    , under = under
-    }
+testRule : String -> Test
+testRule expr =
+    test ("Should report an error when " ++ expr ++ " is found.")
+        (\_ ->
+            source expr
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors [ assertError expr ]
+        )
 
 
 source : String -> String
@@ -54,3 +33,15 @@ module Foo exposing (..)
 foo : Bool -> Bool
 foo bar = 
     if """ ++ expr ++ " then True else False"
+
+
+assertError : String -> ExpectedError
+assertError under =
+    Review.Test.error
+        { message = "Detected a comparison with boolean"
+        , details =
+            [ "There is no need to compare a value of Type Boolean with \"True\" or \"False\""
+            , "For Example: \"if b == True then .. else ..\" is the same as \"if b then ... else ...\", "
+            ]
+        , under = under
+        }
