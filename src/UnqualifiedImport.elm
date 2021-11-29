66849d638688b15d2c190a3bf9f897507ae562e1
Hauke <hauing@pm.me>
2021-11-29 22:48:52 +0100

    Add import of Html child modules to unqualified import example

--- src/UnqualifiedImport.elm
+++ src/UnqualifiedImport.elm
@@ -1,7 +1,11 @@
 module UnqualifiedImport exposing (..)
 
-import List exposing (map)
 import Html exposing (div)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import List exposing (map)
+
 
 a : List Int
-a = map (\i -> i + 3) [1,2,3]
+a =
+    map (\i -> i + 3) [ 1, 2, 3 ]
