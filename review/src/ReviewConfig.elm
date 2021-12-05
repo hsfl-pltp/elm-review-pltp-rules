091341f48ebc946e2d6b8d768190cdcb385186db
Hauke <hauing@pm.me>
2021-12-05 12:46:16 +0100

    Updated comment in reviewConfig

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,13 +1,8 @@
 module ReviewConfig exposing (config)
 
-{-| Do not rename the ReviewConfig module or the config function, because
-`elm-review` will look for these.
+{-| This file configures elm-review
 
-To add packages that contain rules, add them to this review project using
-
-    `elm install author/packagename`
-
-when inside the directory containing this file.
+Please do not change anything here
 
 -}
 
